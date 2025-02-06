5c414dcaf3c6d32abe81e5d80c2a5c3cda747c57
Eirado <111468283+Eirado@users.noreply.github.com>
2025-02-06 15:39:53 -0300

    proj setup

---
+++ GitHubChallenge/GitHubChallenge/GitHubChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubChallengeApp.swift
+//  GitHubChallenge
+//
+//  Created by Gabriel Amaral on 06/02/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubChallenge/GitHubChallenge/Home.swift
@@ -0,0 +1,8 @@
+//
+//  Home.swift
+//  GitHubChallenge
+//
+//  Created by Gabriel Amaral on 06/02/25.
+//
+
+
