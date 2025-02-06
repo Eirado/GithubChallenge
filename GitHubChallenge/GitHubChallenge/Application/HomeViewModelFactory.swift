cbb2ec14fe048102b6e61538000695cb6c326f2d
Eirado <111468283+Eirado@users.noreply.github.com>
2025-02-06 19:21:13 -0300

    HomeViewModelFactory

---
+++ GitHubChallenge/GitHubChallenge/Application/HomeViewModelFactory.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewModelFactory.swift
+//  GitHubChallenge
+//
+//  Created by Gabriel Amaral on 06/02/25.
+//
+
+import Foundation
+
+struct HomeViewModelFactory {
+    
+    static func makeHomeViewModel() -> HomeViewModel {
+        
+        return HomeViewModel()
+    }
+}
--- GitHubChallenge/GitHubChallenge/Presentation/Home/HomeView.swift
+++ GitHubChallenge/GitHubChallenge/Presentation/Home/HomeView.swift
@@ -10,10 +10,6 @@ import SwiftUI
 struct HomeView: View {
     @StateObject private var viewModel: HomeViewModel
     
-    init(viewModel: HomeViewModel) {
-        _viewModel = StateObject(wrappedValue: viewModel)
-    }
-    
     var body: some View {
         NavigationStack {
             VStack {
