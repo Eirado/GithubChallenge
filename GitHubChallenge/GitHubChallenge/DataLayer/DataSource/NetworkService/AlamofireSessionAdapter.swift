b93465d0d21647b385f5084846de4c3ef069fb59
Eirado <111468283+Eirado@users.noreply.github.com>
2025-04-23 20:31:42 -0300

    ,

--- GitHubChallenge/GitHubChallenge/DataLayer/DataSource/NetworkService/AlamofireSessionAdapter.swift
+++ GitHubChallenge/GitHubChallenge/DataLayer/DataSource/NetworkService/AlamofireSessionAdapter.swift
@@ -5,25 +5,25 @@
 //  Created by Gabriel Amaral on 23/04/25.
 //
 
-import Alamofire
-import Foundation
-
-final class AlamofireSessionAdapter: NetworkSessionProtocol {
-    
-    func performRequest(with requestURL: URLRequest) async throws -> (Data, URLResponse) {
-        return try await withCheckedThrowingContinuation { continuation in
-            AF.request(requestURL).validate().responseData { response in
-                switch response.result {
-                case .success(let data):
-                    if let urlResponse = response.response {
-                        continuation.resume(returning: (data, urlResponse))
-                    } else {
-                        continuation.resume(throwing: NetworkError.unknown)
-                    }
-                case .failure:
-                    continuation.resume(throwing: NetworkError.unknown)
-                }
-            }
-        }
-    }
-}
+//import Alamofire
+//import Foundation
+//
+//final class AlamofireSessionAdapter: NetworkSessionProtocol {
+//    
+//    func performRequest(with requestURL: URLRequest) async throws -> (Data, URLResponse) {
+//        return try await withCheckedThrowingContinuation { continuation in
+//            AF.request(requestURL).validate().responseData { response in
+//                switch response.result {
+//                case .success(let data):
+//                    if let urlResponse = response.response {
+//                        continuation.resume(returning: (data, urlResponse))
+//                    } else {
+//                        continuation.resume(throwing: NetworkError.unknown)
+//                    }
+//                case .failure:
+//                    continuation.resume(throwing: NetworkError.unknown)
+//                }
+//            }
+//        }
+//    }
+//}
